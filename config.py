914cb5c48b343c4e75ea3ee6d2f1d397b12b4b81
JezaChen <jeza@vip.qq.com>
2021-11-14 22:09:32 +0800

    重构gunicorn

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-import multiprocessing
-
 bind = "0.0.0.0:5000"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 4
 accesslog = 'access.log'  # 访问日志目录
-errorlog  = 'error.log'  # 错误日志目录
+errorlog = 'error.log'  # 错误日志目录
 capture_output = True  # 重定向标准输出到错误日志。默认为False。
 timeout = 600  # 过期时间
 loglevel = "debug"  # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
