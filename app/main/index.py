a9f6bd51364781b2d3401ddb80b7d38792395672
JezaChen <jeza@vip.qq.com>
2021-11-14 21:49:13 +0800

    参数过长跨域问题解决尝试

--- app/main/index.py
+++ app/main/index.py
@@ -4,3 +4,9 @@ from . import main
 @main.route('/', methods=['GET'])
 def index():
     return '<h1>Bad Request</h1>', 400
+
+
+@main.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
