9a058ce8fef2db0292d8cf4ea5df96fcbe481aa7
JezaChen <jeza@vip.qq.com>
2021-11-14 21:52:17 +0800

    尝试失败，回滚

--- app/main/index.py
+++ app/main/index.py
@@ -4,9 +4,3 @@ from . import main
 @main.route('/', methods=['GET'])
 def index():
     return '<h1>Bad Request</h1>', 400
-
-
-@main.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
